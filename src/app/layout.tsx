a5daa17aab7f993fd965271495ae7e0222a954bd
Joel Miller <jmill266@uwo.ca>
2024-08-28 17:56:19 -0400

    Configuring static exports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
@@ -10,11 +9,11 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
+  title: `Joel' Miller`,
   description: `A website I have created to promote my personal brand:).`,
-  openGraph: {
-    images: [HOME_OG_IMAGE_URL],
-  },
+  // openGraph: {
+  //   images: [HOME_OG_IMAGE_URL],
+  // },
 };
 
 export default function RootLayout({
