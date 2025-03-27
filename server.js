e238d8ccb02efbe8d5be3e133a569d6f92a84a6f
Joel Miller <jmill266@uwo.ca>
2025-03-27 19:47:08 -0400

    Trying this now 😂

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the current directory
-app.use(express.static(__dirname));
-
-// Serve the mnist_model directory
-app.use('/mnist_model', express.static(path.join(__dirname, 'mnist_model')));
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Listen on all available network interfaces
