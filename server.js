e5bc252a23b8514724ead6d9235935cef0f41c0c
Joel Miller <jmill266@uwo.ca>
2025-03-27 19:53:52 -0400

    Maybe this?

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Handle 404s
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Listen on all available network interfaces
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
+    console.log(`Static files being served from: ${path.join(__dirname, 'public')}`);
 }); 
